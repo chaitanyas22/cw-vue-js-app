29586daac41272139801ffd4c2126be3464d7413
Chaitanya Sharma <62926067+chaitanyas22@users.noreply.github.com>
2024-12-02 11:10:47 +0000

    Creared Lessons vue file in components that will fetch lessons from back end API

---
+++ src/components/lessons.vue
@@ -0,0 +1,37 @@
+<!-- src/components/Lessons.vue -->
+<template>
+    <div>
+      <h1>Lessons from the Backend</h1>
+      <ul v-if="lessons.length">
+        <li v-for="lesson in lessons" :key="lesson._id">
+          {{ lesson.name }}: {{ lesson.description }}
+        </li>
+      </ul>
+      <p v-else>Loading lessons...</p>
+
+
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        lessons: []  // Array to store lessons data
+      };
+    },
+    mounted() {
+        
+      // Fetch lessons data from your Express backend
+      fetch('https://your-express-api-url/api/lessons')
+        .then(response => response.json())
+        .then(data => {
+          this.lessons = data;  // Assign the fetched data to lessons array
+        })
+        .catch(error => {
+          console.error('Error fetching lessons:', error);
+        });
+    }
+  };
+  </script>
+  
